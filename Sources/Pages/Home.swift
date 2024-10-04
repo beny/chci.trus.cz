c94abb1924284a3aa0191808af7204ad1d86dc8f
Ondřej Beneš <ondrej.benes@airbank.cz>
2024-10-04 08:47:32 +0200

    Init commit

---
+++ Sources/Pages/Home.swift
@@ -0,0 +1,56 @@
+import Foundation
+import OpenGraph
+import Ignite
+
+struct Home: StaticPage {
+    var title = "chci"
+
+    private struct Item {
+        let title: String?
+        let description: String?
+        let imageURL: String?
+        let url: String?
+    }
+
+    func body(context: PublishingContext) async -> [BlockElement] {
+        let items = await fetchItems(context: context)
+        Section {
+            for item in items {
+                Card(imageName: item.imageURL) {
+                    Text(item.title ?? "")
+                        .font(.title1)
+                    Text(item.description ?? "")
+
+                    Link("Detail", target: item.url ?? "")
+                        .linkStyle(.button)
+                }
+                .frame(width: 400)
+            }
+        }
+        .padding()
+    }
+
+    private func fetchItems(context: PublishingContext) async -> [Item] {
+        let links = context.decode(resource: "links.json", as: [String].self) ?? []
+        var items: [Item] = []
+        do {
+            for link in links {
+                guard let url = URL(string: link) else { continue }
+                let openGraph = try await OpenGraph.fetch(url: url)
+                dump(openGraph)
+                items.append(
+                    Item(
+                        title: openGraph[.title],
+                        description: openGraph[.description],
+                        imageURL: openGraph[.image],
+                        url: openGraph[.url]
+                    )
+                )
+            }
+        } catch {
+            print(error.localizedDescription)
+        }
+
+        return items
+    }
+}
---
+++ Sources/Site.swift
@@ -0,0 +1,30 @@
+import Foundation
+import Ignite
+
+@main
+struct IgniteWebsite {
+    static func main() async {
+        let site = ExampleSite()
+
+        do {
+            try await site.publish()
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+}
+
+struct ExampleSite: Site {
+    var name = "chci"
+    var url = URL("https://chci.trus.cz")
+    var builtInIconsEnabled = true
+
+    var author = "Ondra Beneš"
+
+    var favicon = URL(string: "/images/favicon.ico")
+
+    var homePage = Home()
+    var theme = MyTheme()
+}
+
+
